789e8663e08b63fe6cb12a3575cf276a24e4b78b
Christopher Yuan <christopheryuan@A1277-MBP15.tc.getpebble.com>
2015-07-02 15:46:27 -0700

    3

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 rm -rf ./job/*.job
-python aggregate_tables.py DropAllTables --workers 20 --local-scheduler
-python aggregate_tables.py AggregateTableRun --workers 10 --local-scheduler
+python ./aggregate_tables.py DropAllTables --workers 20 --local-scheduler
+python ./aggregate_tables.py AggregateTableRun --workers 10 --local-scheduler
